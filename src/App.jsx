25c7b4235679a3cd4060ac5b43c297483350dc4c
Aakash Acharya <mail.aakash108@gmail.com>
2024-09-01 19:20:59 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Gallery from './components/Gallery'
+
+export default function App() {
+  
+
+  return (
+   
+     <Gallery />
+      
+
+    
+  )
+}
+
+
