35c5c4894f6773e482e343f9d747592382e5eeae
Aakash Acharya <mail.aakash108@gmail.com>
2024-09-13 23:41:23 +1000

    managing state

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,28 @@ import {sculptureList} from "./components/data";
 
 const App = () => {
   const [index,setIndex] = useState(0)
-  let sculpture = sculptureList
+  const [display,setDisplay] = useState(true) 
+  let sculpture = sculptureList[index]
 
   function onButtonClick(){
     setIndex(index+1)
+    console.log(sculpture.length)
+    if(index===sculptureList.length-1){
+      setIndex(0)
+    }
 
+  }
+
+  function toggleShowMore(){
+    setDisplay(!display)
   }
   return (
     <div>
       <button onClick={onButtonClick}>Next</button>
-      <h2>{sculpture[index].name} by <i>{sculpture[index].artist}</i></h2>
-      <p>{index} of {sculpture.length}</p>
+      <h2>{sculpture.name} by <i>{sculpture.artist}</i></h2>
+      <p>{index+1} of {sculptureList.length}</p>
+      <button onClick={toggleShowMore}>{display?"ShowDetails":"HideDetails"}</button>
+      {!display&&<p>{sculpture.description}</p>}
     </div>
   )
 }
