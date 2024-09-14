5a9a9d7f9e2d2f588678020d028a30efbdcbe50f
Aakash Acharya <mail.aakash108@gmail.com>
2024-09-14 15:25:05 +1000

    filter in react

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,40 @@ import React from 'react'
 import { useState } from 'react'
 
 const App = () => {
-  const [person, setPerson] = useState({
-    name: "Aakash Acharya",
-    city: "Kathmandu",
-    score:10
-  })
+  const [person, setPerson] = useState([
+    {
+      id: 1,
+      name: "Marta Colvin Andrade"
+    },
+    {
+      id: 2,
+      name: "Lamidi Olonade Fakeye"
+    },
+    {
+      id: 3,
+      name: "Louise Nevelson"
+    },
 
-  function handlePlusClick(){
-    setPerson({...person, score: person.score+1
-    })
+  ])
 
-  }
-
-  function handleOnChange(e){
-    const {name,value} = e.target
-    console.log(name,value)
-    setPerson({...person,[name]: value})
-
+  function handleDelete(id){
+    const data = person.filter((element)=>
+      element.id !== id
+    )
+    console.log(data)
+    setPerson(data)
   }
   return (
     <div>
-      <label>Score: <b>{person.score}</b>{" "}<button onClick={handlePlusClick}>+1</button></label>
-      <label>Name: </label>
-      <input type="text" name='name' value={person.name} onChange={handleOnChange} />
+      <ul>
+        {person.map(element=>(
+          <li key={element.id}>{element.name} <button onClick={()=>handleDelete(element.id)}>Delete</button></li>
+        ))}
+      </ul>
 
-
-      <label>City: </label>
-      <input type="text" name='city' value={person.city} onChange={handleOnChange} />
-      <p>{person.name}</p>
-      <p>{person.city}</p>
-      <p>{person.score}</p>
     </div>
   )
 }
 
 export default App
+
