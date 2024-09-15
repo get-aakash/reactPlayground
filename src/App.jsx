b93b172a46acc655ab5ebbf3482b81945e802d6f
Aakash Acharya <mail.aakash108@gmail.com>
2024-09-15 21:07:10 +1000

    avoid contradictions in state

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,36 @@
 import React from 'react'
 import { useState } from 'react'
 
-let nextId = 3
-
-  const initialList = [
-    {
-      id:0, title: "Big Bellies", seen: false
-    },
-    {
-      id:1, title: "Lunar Landscape", seen: false
-    },
-    {
-      id:2, title: "Terracotta Army", seen: true
-    }
-  ]
 const App = () => {
-  const [myList, setMyList] = useState(initialList)
-  const [yourList, setYourList] = useState(initialList)
+  const [answer, setAnswer] = useState("")
+  const [isSending, setIsSending] = useState(false)
+  const [isSent, setIsSent] = useState(false)
 
-  function handleOnToggleMyList(artworkId,nextSeen){
-    const myNextList = [...myList];
-    const artwork = myNextList.find(
-      a => a.id === artworkId
-    );
-    artwork.seen = nextSeen;
-    setMyList(myNextList);
-
+  async function handleOnSubmit(e){
+    e.preventDefault()
+    setIsSending(true)
+    await sendMessage(answer)
+    setIsSending(false)
+    setIsSent(true)
   }
-  function handleOnToggleYourList(artworkId, nextSeen){
-    const yourNextList = [...yourList];
-    const artwork = yourNextList.find(
-      a => a.id === artworkId
-    );
-    artwork.seen = nextSeen;
-    setYourList(yourNextList);
-
+  if(isSent){
+    return <h1>Thank you for the feedback!!</h1>
   }
-
-  
   return (
     <div>
-      <h1>Art Bucket List</h1>
-      <h2>My List of Art to see:</h2>
-      <ItemList artworks={myList} onToggle={handleOnToggleMyList} />
-      <h2>Your List of Art here</h2>
-      <ItemList artworks={yourList} onToggle={handleOnToggleYourList} />
-      
+      <h3>How was your stay at The Prancing Pony?</h3>
+      <textarea disabled={isSending} onChange={e=>setAnswer(e.target.value)} />
+      <br />
+      <button onClick={handleOnSubmit} disabled={isSending}>Send</button>
+      {isSending && <p>Sending...</p>}
     </div>
   )
 }
 
-function ItemList({artworks, onToggle}){
-  return(
-    <ul>
-      {artworks.map(artwork => (
-        <li key={artwork.id}>
-          <label>
-            <input
-              type="checkbox"
-              checked={artwork.seen}
-              onChange={e => {
-                onToggle(
-                  artwork.id,
-                  e.target.checked
-                );
-              }}
-            />
-            {artwork.title}
-          </label>
-        </li>
-      ))}
-    </ul>
-  )
+function sendMessage(answer){
+  return new Promise (resolve=>{
+    setTimeout(resolve, 2000)
+  })
 }
 
 export default App
