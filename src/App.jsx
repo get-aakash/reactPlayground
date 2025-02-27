e2789f5c31fa9f7a1dab20690eb14e5dd3b811bb
Aakash Acharya <mail.aakash108@gmail.com>
2025-02-27 19:49:29 +1100

    list and map

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import MyButton from './components/MyButton'
+
+function App() {
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <MyButton />
+    </>
+
+  )
+}
+
+export default App
---
+++ src/components/MyButton.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const MyButton = () => {
+    const user = {
+        name: "Aakash Acharya",
+        imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+        imageSize: 90
+    }
+
+    const myVariable = [
+        {
+            name: "Banana",
+            id : 1
+        },
+        {
+            name: "Apple",
+            id : 2
+        },
+
+    ]
+    const onClickHandle = (fruitName)=>{
+        alert(`You clicked on: ${fruitName}`)
+
+    }
+
+    const listedItems = myVariable.map((item)=>
+        <li key={item.id} onClick={()=>onClickHandle(item.name)}>{item.name}</li>
+    )
+    return (
+        <div>
+            <button>This is a Button</button>
+            <h1>{user.name}</h1>
+            <img className='avatar' src={user.imageUrl} style={{ height: user.imageSize, width: user.imageSize }} alt="" />
+
+            <ul>{listedItems}</ul>
+        </div>
+    )
+}
+
+export default MyButton
