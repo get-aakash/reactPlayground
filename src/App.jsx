efabcf4d97d4a0ec28ef581c17b247e8b24412cd
Aakash Acharya <mail.aakash108@gmail.com>
2025-07-23 00:11:02 +1000

    edit the data

--- src/App.jsx
+++ src/App.jsx
@@ -21,21 +21,21 @@ function App() {
 
         />
          <Contact 
-        img="src/assets/mr-whiskerson.png"
-        name="Mr. Whiskerson"
-        phone="(212) 555-1234"
-        email="mr.whiskaz@catnap.meow"
+        img="src/assets/fluffykins.png"
+        name="Fluffykins"
+        phone="(212) 555-2345"
+        email="fluff@me.com"
 
         />
          <Contact 
-        img="src/assets/mr-whiskerson.png"
-        name="Mr. Whiskerson"
-        phone="(212) 555-1234"
-        email="mr.whiskaz@catnap.meow"
+        img="src/assets/felix.png"
+        name="Felix"
+        phone="(212) 555-4567"
+        email="thecat@hotmail.com"
 
         />
         <Contact 
-        img="src/assets/mr-whiskerson.png"
+        img="src/assets/pumpkin.png"
         name="Mr. Whiskerson"
         phone="(212) 555-1234"
         email="mr.whiskaz@catnap.meow"
