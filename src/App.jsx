5280f32eaa5a5385c4039051145ea19936f9edae
Aakash Acharya <mail.aakash108@gmail.com>
2025-08-27 23:57:20 +1000

    mp3 sound on data

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,6 @@ function App() {
     <>
      <Pads />
 
-      
-      
-
-
     </>
   )
 
--- src/component/Pads.jsx
+++ src/component/Pads.jsx
@@ -1,21 +1,15 @@
 import React from 'react'
-import data from '../data'
+import data from "../data"
 
 const Pads = () => {
-    const [soundData, setSoundData] = React.useState(data)
-    
-    function toggleColor(id){
-      setSoundData(soundData.map((item)=>{
-        return(
-          item.id === id? {...item, on:!item.on}: item
-        )
-      }))
-      
-      
-    }
   return (
     <div className='pad-container'>
-        {soundData.map((item)=><button  key={item.id} onClick={()=>toggleColor(item.id)} style={{backgroundColor:item.on?item.color:""}}>{item.color}</button>)}
+        {data.map((item)=>{
+            return(
+                <div className="sound-pad" style={{backgroundColor: item.color}}>{item.color}</div>
+            )
+        })}
+        
       
     </div>
   )
