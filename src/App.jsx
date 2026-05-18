e9c36492d536f86f00bdc87f1b838abdfb9f1625
Aakash Acharya <mail.aakash108@gmail.com>
2026-05-19 00:04:31 +1000

    react-redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,12 @@
-import React, { useEffect, useLayoutEffect } from 'react'
-import { fetchUser } from './axios'
-import { Button, Card } from 'react-bootstrap'
+
+import PizzaBox from './component/PizzaBox'
 
 const App = () => {
-    const [userData, setUserData] = React.useState([])
-    const [loading, setLoading] = React.useState(true)
-    const [error, setError] = React.useState(null)
-    const [search, setSearch] = React.useState("")
-
-    const handleOnChange = (e)=>{
-        setSearch(e.target.value.toLowerCase())
-        
-
-    }
-    
-   const filteredUsers = userData.filter(user =>
-    user.name.toLowerCase().includes(search)
-  )
-    
-    
-    
-    useEffect(() => {
-        const loadData = async () => {
-            setLoading(true)
-            try {
-                const data = await fetchUser()
-                setUserData(data)
-            } catch (error) {
-                setError("Failed to load user")
-
-            } finally {
-                setLoading(false)
-            }
-
-        }
-        loadData()
-
-
-    }, [])
+ 
     return (
-        <div>
-            <div className="serach-container">
-                <input type="text" placeholder='search here...'  onChange={handleOnChange}/>
-            </div>
-            {
-                filteredUsers.map((user) => {
-                    return (
-                        <Card key={user.id} style={{ width: '18rem' }}>
-                            
-                            <Card.Body>
-                                <Card.Title>{user.name}</Card.Title>
-                                <Card.Text>
-                                    {user.email}
-                                </Card.Text>
-                                
-                            </Card.Body>
-                        </Card>
-                    )
-                })
-            }
+      <PizzaBox />
 
-        </div>
+        
     )
 }
 
